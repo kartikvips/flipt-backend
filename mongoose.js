13f4a6eb95a953f9f5db33518a1fc9658410ea86
FelixAlvarado <felixalvarado653@gmail.com>
2018-06-25 18:24:21 -0700

    modified post requrest

---
+++ mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require ('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose
+    .connect('mongodb://localhost/test')
+    .then(()=> console.log('DB connected'))
+    .catch(err=>console.log(err));
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('mongoose connected');
+});
+
+module.exports = mongoose;
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -7,7 +7,7 @@ var router = express.Router();
      router.post('/api/books', (req,res) => {
         const {title,author, isbn, year, description, imageUrl, category} = req.body;
 
-        const book = new Book({
+        const newBook = new Book({
             title,
             author,
             isbn,
@@ -17,8 +17,30 @@ var router = express.Router();
             category
         });
 
-        book.save();
-        res.send(book);
+        // res.send(newBook);
+        let test =  newBook.save();
+        debugger;
+        test.then(console.log,console.log);
+        // .then((book) => {
+        //     console.log(book);
+        //     if(!book){
+        //        return res.status(400).send();
+        //     }
+        //     return res.status(201).send(book);
+        // }).catch(err => {
+        //     if(err){
+        //         return res.status(400).send({error:err});
+        //     }
+        //     return res.status(400).send();
+        // });
+
+            
+     
      });
+
+     router.get('/api/book/:id', (req,res) => {
+        
+     });
+    
 
      module.exports = router;
