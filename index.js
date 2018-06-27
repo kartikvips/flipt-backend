ac527a801e625f9d5b5cf2ea93899396c5f02ec3
kartikvips <kartikvips@gmail.com>
2018-06-27 09:41:37 -0700

    fix index.js route

--- index.js
+++ index.js
@@ -5,6 +5,42 @@ const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
+const booksRouter = require('./routes/bookRoutes');
+const bodyParser = require('body-parser');
+
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+app.use(bodyParser.json());
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+const booksRouter = require('./routes/bookRoutes');
+const bodyParser = require('body-parser');
 
 mongoose.connect(keys.mongoURI);
 
@@ -21,7 +57,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-
+app.use('/books', booksRouter);
 require('./routes/authRoutes')(app);
 
 
